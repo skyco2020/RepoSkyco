1d29a7ae463af4659140828c712a4c6db4903f44
pradel <prade5_16@hotmail.com>
2020-06-14 12:58:06 -0300

    bd

--- SkycoApi/BusinessEntities/BE/PaymentBE.cs
+++ SkycoApi/BusinessEntities/BE/PaymentBE.cs
@@ -15,7 +15,6 @@ namespace BusinessEntities.BE
         public String Description { get; set; }
         public String Currency { get; set; }
         public Int32 Quantity { get; set; }
-
         #region List
         public List<Payment_Skyco_AccountBE> Payment_Skyco_Accounts { get; set; }
         #endregion
